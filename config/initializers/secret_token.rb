c4237b5a49f93a7d94b7c374286ec24a11c516a3
vytarani <vytarani.mathane@gmail.com>
2010-07-29 16:50:05 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yaapp::Application.config.secret_token = 'eb9a1a80b6b0bbaaf3b1c6c3dbb2a9cc56630540bc7a361c99c0bec7c1c779fbfc29b21e804ba72ad3133b50f69634c0038672131af8ceb8d1b453dd2b771b40'
